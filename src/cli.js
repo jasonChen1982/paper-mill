c3e6eec1e13967a9f39bd7f674c0c80287fb215f
jason chen <cjx89807@alibaba-inc.com>
2017-04-05 17:49:51 +0800

    chore: remove something

--- src/cli.js
+++ src/cli.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const path = require('path');
-const chalk = require('chalk');
-const fullname = require('fullname');
-const inquirer = require('inquirer');
-const moment = require('moment');
-const ejs = require('ejs');
+// const fs = require('fs');
+// const path = require('path');
+// const chalk = require('chalk');
+// const fullname = require('fullname');
+// const inquirer = require('inquirer');
+// const moment = require('moment');
+// const ejs = require('ejs');
 const util = require('./util');
-const cwd = process.cwd();
+// const cwd = process.cwd();
 
 util.updateCheck();
